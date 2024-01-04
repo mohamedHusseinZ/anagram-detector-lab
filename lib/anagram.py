0f88f4b5c6e25caa5ef50f42d849a45970d7c7bc
Rahy-Sam <raymond.mwaiki@student.moringaschool.com>
2024-01-04 07:34:17 -0800

    first commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  # Convert the word to lowercase for case-insensitive comparison
+
+    def match(self, word_list):
+        return [w for w in word_list if self.is_anagram(w)]
+
+    def is_anagram(self, other_word):
+        # Check if two words are anagrams
+        return sorted(self.word) == sorted(other_word.lower()) and self.word != other_word.lower()
--- lib/debug.py
+++ lib/debug.py
@@ -4,3 +4,5 @@ from anagram import Anagram
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+listen = Anagram("listen")
+result = listen.match(['enlists', 'google'])
